b1cf647010495febe693f8abe64fba7849ab55c8
santidelgadof <92078732+santidelgadof@users.noreply.github.com>
2024-02-18 12:43:34 +0100

    Contornos detectados (Primer código)
    Detecta la mayor parte de los contornos

--- Contornos.py
+++ Contornos.py
@@ -1,27 +1,41 @@
-# Importamos las librerías necesarias
 import cv2
 import numpy as np
 
 # Leemos la imagen que nos pasas
-imagen = cv2.imread("Foto_contorno.jpg")
+imagen = cv2.imread("Foto_contorno3.jpg")
 
 # Convertimos la imagen a espacio de color HSV
 hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
 
 # Definimos el rango de color amarillo en HSV
-amarillo_bajo = np.array([30, 125, 150])
+amarillo_bajo = np.array([30, 100, 130])
 amarillo_alto = np.array([30, 255, 255])
 
-# Creamos una máscara con solo los pixeles dentro del rango de amarillo
+# Creamos una máscara binaria con solo los píxeles dentro del rango de amarillo
 mascara = cv2.inRange(hsv, amarillo_bajo, amarillo_alto)
 
-# Encontramos los contornos en la máscara
-contornos, _ = cv2.findContours(mascara, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# Aplicamos un umbral binario a la máscara con valor 125
+_, umbralizada = cv2.threshold(mascara, 80, 255, cv2.THRESH_BINARY)
+
+# Encontramos los contornos en la imagen umbralizada
+contornos, _ = cv2.findContours(umbralizada, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 # Dibujamos los contornos sobre la imagen original
 cv2.drawContours(imagen, contornos, -1, (0, 0, 255), 2)
 
-# Mostramos el resultado
+# Contamos el número de contornos encontrados
+num_contornos = len(contornos)
+
+# Si el número de contornos es mayor que cero, significa que hay enemigos (Se modificará en un futuro)
+if num_contornos > 0:
+    print(f"Se detectaron {num_contornos} contornos amarillos")
+    print(f"Hay enemigos en la imagen")
+else:
+    print("No hay enemigos en la imagen")
+
+# Resultado
 cv2.imshow("Resultado", imagen)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+

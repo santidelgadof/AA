8faa7972bef0a1ccbca06ffbe4682bd02e2f25b7
Antonio Sanjurjo Rodriguez <a.sanjurjo1@udc.es>
2024-03-23 20:27:29 +0100

    pero si esto entrena y todo

--- red.jl
+++ red.jl
@@ -2,9 +2,10 @@ using Flux
 using Statistics
 using FileIO;
 using DelimitedFiles;
+include("redfuncs.jl")
 
 
-learningRate = 0.03
+
 
 
 dataset = readdlm("datos.data",',');
@@ -13,20 +14,34 @@ inputs = dataset[:,1:2];
 inputs = Float32.(inputs);
 inputs = hcat(inputs);
 targets = dataset[:,3];
-targets = Float32.(targets);
+targets = Bool.(targets);
 targets =hcat(targets);
-#targets = reshape(targets.==classes[1], :, 1);
 
-targets
+
+#ann = Chain(
+# Dense(2, 4, σ),
+# Dense(4, 1, σ) );
+#
+#loss(ann,x, y) = Flux.Losses.binarycrossentropy(ann(x), y)
+
+
+#opt_state = Flux.setup(Adam(learningRate), ann)
+# apotrofe traspone matriz
+#Flux.train!(loss, ann, [(inputs', targets')], opt_state)
+
+
+
+numInputs = 2
+topology = [4]
+transferFuncs = fill(σ, length(topology))
+epoches = 10
+minLoss = 0.0
+learningRate = Float32.(0.03)
+
+(ann, losses) = trainANN(topology, (inputs, targets), transferFuncs, epoches, minLoss, learningRate)
+
+print("\n\nITER\n")
+print(targets)
+print("\n\nINPUTS\n")
 inputs
 
-ann = Chain(
- Dense(2, 4, σ),
- Dense(4, 1, σ) );
-
-loss(ann,x, y) = Flux.Losses.binarycrossentropy(ann(x), y)
- 
-
-opt_state = Flux.setup(Adam(learningRate), ann)
-# apotrofe traspone matriz
-Flux.train!(loss, ann, [(inputs', targets')], opt_state)
---
+++ redfuncs.jl
@@ -0,0 +1,61 @@
+
+function buildANN(numInputs::Int, topology::AbstractArray{<:Int,1}, numOutputs::Int, transferFunctions::AbstractArray{<:Function,1})
+    ann=Chain();
+    numInputsLayer = numInputs;
+    for numHiddenLayer in 1:length(topology)
+        numNeurons = topology[numHiddenLayer];
+        ann = Chain(ann..., Dense(numInputsLayer, numNeurons, transferFunctions[numHiddenLayer]));
+        numInputsLayer = numNeurons;
+    end;
+    if (numOutputs == 1)
+        ann = Chain(ann..., Dense(numInputsLayer, 1, σ));
+    else
+        ann = Chain(ann..., Dense(numInputsLayer, numOutputs, identity));
+        ann = Chain(ann..., softmax);
+    end;
+    return ann;
+end;
+
+function trainANN(topology::AbstractVector{<:Int64}, dataset::Tuple{AbstractMatrix{<:Float32}, AbstractMatrix{Bool}},
+    transferFunctions::AbstractVector{<:Function}, maxEpochs::Int64=1000, minLoss::Real=0.0, learningRate::Real=0.01)
+
+    (inputs, targets) = dataset;
+
+    # Creamos la RNA
+    ann = buildANN(size(inputs,2), topology, size(targets,2), transferFunctions);
+
+    # Definimos la funcion de loss
+    loss(x,y) = (size(y,1) == 1) ? Flux.Losses.binarycrossentropy(ann(x),y) : Flux.Losses.crossentropy(ann(x),y);
+
+    # Creamos los vectores con los valores de loss y de precision en cada ciclo
+    trainingLosses = Float32[];
+
+    # Empezamos en el ciclo 0
+    numEpoch = 0;
+    # Calculamos el loss para el ciclo 0 (sin entrenar nada)
+    trainingLoss = loss(inputs', targets');
+    #  almacenamos el valor de loss y precision en este ciclo
+    push!(trainingLosses, trainingLoss);
+    #  y lo mostramos por pantalla
+    println("Epoch ", numEpoch, ": loss: ", trainingLoss);
+
+    # Entrenamos hasta que se cumpla una condicion de parada
+    while (numEpoch<maxEpochs) && (trainingLoss>minLoss)
+
+        # Entrenamos 1 ciclo. Para ello hay que pasar las matrices traspuestas (cada patron en una columna)
+        Flux.train!(loss, Flux.params(ann), [(inputs', targets')], ADAM(learningRate));
+
+        # Aumentamos el numero de ciclo en 1
+        numEpoch += 1;
+        # Calculamos las metricas en este ciclo
+        trainingLoss = loss(inputs', targets');
+        #  almacenamos el valor de loss
+        push!(trainingLosses, trainingLoss);
+        #  lo mostramos por pantalla
+        println("Epoch ", numEpoch, ": loss: ", trainingLoss);
+
+    end;
+
+    # Devolvemos la RNA entrenada y el vector con los valores de loss
+    return (ann, trainingLosses);
+end;

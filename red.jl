999b164217ff99f053a18b390595437b9af0ab5e
Antonio Sanjurjo Rodriguez <a.sanjurjo1@udc.es>
2024-03-24 20:44:40 +0100

    quitar mierda

--- red.jl
+++ red.jl
@@ -1,10 +1,6 @@
 include("librerias.jl")
 include("redfuncs.jl")
 
-
-
-
-
 dataset = readdlm("datos.data",',');
 
 inputs = dataset[:,1:2];
@@ -13,20 +9,6 @@ inputs = hcat(inputs);
 targets = dataset[:,3];
 targets = Bool.(targets);
 targets =hcat(targets);
-
-
-#ann = Chain(
-# Dense(2, 4, σ),
-# Dense(4, 1, σ) );
-#
-#loss(ann,x, y) = Flux.Losses.binarycrossentropy(ann(x), y)
-
-
-#opt_state = Flux.setup(Adam(learningRate), ann)
-# apotrofe traspone matriz
-#Flux.train!(loss, ann, [(inputs', targets')], opt_state)
-
-
 
 numInputs = 2
 topology = [4]

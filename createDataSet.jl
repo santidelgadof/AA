a7a0a210375c4e4f827340202b073d92670833fc
Antonio Sanjurjo Rodriguez <a.sanjurjo1@udc.es>
2024-03-25 12:31:38 +0100

    facilidades

--- createDataSet.jl
+++ createDataSet.jl
@@ -1,17 +1,22 @@
 include("contornos.jl")
 
-function createDataSet(sourcePath, dataSetSize, filename)
+function createDataSet(sourcePath, dataSetSize, filename, idTags)
     file = open(filename, "w")
     
     
     for i in 0:dataSetSize
-        newpath = sourcePath * "frame_$(i).jpg"
-        println("\nNOW PROCESSING IMAGE $(i)\n")
+        imageFile = "frame_$(i).jpg"
+        newpath = sourcePath * imageFile
+        println("NOW PROCESSING IMAGE $(i)\n")
         local data = processImage(newpath)
     
         for element in data
             # Escribir datos
-            write(file, "$(element[3]) $(element[4]), 0\n")
+            if !idTags
+                write(file, "$(element[3]) $(element[4]), 0\n")
+            else
+                write(file, "$(element[3]) $(element[4]), 0  \t\t| IMAGE: $i BOX: $(element[2])\n")
+            end
         end
     end
     
--- red.jl
+++ red.jl
@@ -5,8 +5,9 @@ include("createDataSet.jl")
 filename = "datos.txt"
 sourcePath = "Frames/frames1/"
 dataSetSize = 4
+idTags = true
 
-createDataSet(sourcePath,dataSetSize, filename)
+createDataSet(sourcePath, dataSetSize, filename, idTags)
 
 dataset = readdlm("datos.data",',');
 

4df21f00d21bb1b706e57d585b4161d0714399f1
santidelgadof <92078732+santidelgadof@users.noreply.github.com>
2024-03-29 11:10:33 +0100

    Update libreries

--- librerias.jl
+++ librerias.jl
@@ -1,5 +1,16 @@
-using Images, ImageFiltering, FileIO, ImageView, ImageMorphology, ColorTypes, ImageDraw, Colors
+using Images, ImageFiltering, ImageView, ImageMorphology, ColorTypes, ImageDraw, Colors
 using Flux
 using Statistics
 using FileIO;
 using DelimitedFiles;
+using DataFrames
+using Flux.Losses
+using Random
+using Random:seed!
+using Plots
+using Clustering
+using ScikitLearn
+using StatsBase
+using ImageFeatures
+using ImageTransformations
+using CUDA

44055575355d268e582519f3d0445e28e24c9304
alfredo1700 <david.carballo1@udc.es>
2024-04-13 12:40:26 +0200

    results 3 aprox
    Co-authored-by: alvarocainzos <alvarocainzos@users.noreply.github.com>

--- main.jl
+++ main.jl
@@ -13,8 +13,8 @@ numRepetitionsANNTraining = 50;
 
 kernel = "sigmoid";
 kernelDegree = 3;
-kernelGamma = 4;
-C=10;
+kernelGamma = 5;
+C=20;
 maxDepth = 21;
 
 numNeighbors = 19;
@@ -27,34 +27,34 @@ targets = dataset[:,end];
 
 crossValidationIndices = crossvalidation(targets, numFolds);
 #normalizeZeroMean!(inputs);
-
+#si hay dos que sea separado con coma [4,2]
 topology = [32,20]
 topology_string = join(topology,"+")
 
 println("Comenzando entrenamiento con topología [$topology_string]")
 
-dirname = "aprox_2"
+dirname = "aprox_3"
 output_name = dirname*"/$topology_string-output.txt"
 
 if !isdir(dirname)
     mkdir(dirname);
 end
 
-modelHyperparameters = Dict();
-modelHyperparameters["topology"] = topology;
-modelHyperparameters["learningRate"] = learningRate;
-modelHyperparameters["validationRatio"] = validationRatio;
-modelHyperparameters["numExecutions"] = numRepetitionsANNTraining;
-modelHyperparameters["maxEpochs"] = numMaxEpochs;
-modelHyperparameters["maxEpochsVal"] = maxEpochsVal;
-
-
-open(output_name,"w+") do out
-    redirect_stdout(out) do
-        println("RNA ----------------\n")
-        @time "Elapsed time" modelCrossValidation(:ANN, modelHyperparameters, inputs, targets, crossValidationIndices);
-    end
-end
+#modelHyperparameters = Dict();
+#modelHyperparameters["topology"] = topology;
+#modelHyperparameters["learningRate"] = learningRate;
+#modelHyperparameters["validationRatio"] = validationRatio;
+#modelHyperparameters["numExecutions"] = numRepetitionsANNTraining;
+#modelHyperparameters["maxEpochs"] = numMaxEpochs;
+#modelHyperparameters["maxEpochsVal"] = maxEpochsVal;
+#
+#
+#open(output_name,"w+") do out
+#    redirect_stdout(out) do
+#        println("RNA ----------------\n")
+#        @time "Elapsed time" modelCrossValidation(:SVM, modelHyperparameters, inputs, targets, crossValidationIndices);
+#    end
+#end
 
 
 modelHyperparameters = Dict();
@@ -64,7 +64,7 @@ modelHyperparameters["kernelGamma"] = kernelGamma;
 modelHyperparameters["C"] = C;
 #println("\nSVM ----------------\n")
 #modelCrossValidation(:SVM, modelHyperparameters, inputs, targets, crossValidationIndices);
-#println("\nDT ----------------\n")
-#modelCrossValidation(:DecisionTree, Dict("maxDepth" => maxDepth), inputs, targets, crossValidationIndices);
+println("\nDT ----------------\n")
+modelCrossValidation(:DecisionTree, Dict("maxDepth" => maxDepth), inputs, targets, crossValidationIndices);
 #println("\nkNN ----------------\n")
 #modelCrossValidation(:kNN, Dict("numNeighbors" => numNeighbors), inputs, targets, crossValidationIndices);

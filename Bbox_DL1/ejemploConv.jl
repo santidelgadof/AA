73da3defd7349e445fb153ffd7b7a44d2c5c8c8b
Alvaro Cainzos <alvaro.cainzos@udc.es>
2024-05-05 18:54:24 +0200

    aprox1_DL

---
+++ Bbox_DL1/ejemploConv.jl
@@ -0,0 +1,195 @@
+using Flux
+using Flux.Losses
+using Flux: onehotbatch, onecold
+using JLD2, FileIO
+using Statistics: mean
+using Random
+using Random:seed!
+
+seed!(1);
+
+train_labels = [];
+test_labels = [];
+testRatio = 10;
+labels = 0:1;
+train_imgs = Array{Float32}(undef, 100, 75, 1, 0);
+test_imgs = Array{Float32}(undef, 100, 75, 1, 0);
+
+function loadImages(testRatio)
+    
+    dirNeg = "Bbox_DL1/No-enemigos/";
+    dirPos = "Bbox_DL1/Enemigos/";
+    auxTestImgs = [];
+    auxTrainImgs = [];
+    
+
+    n = 1;
+
+    for file in readdir(dirNeg)
+        image = load("$dirNeg$file");
+        if (n % testRatio == 0) 
+            push!(auxTestImgs, image);
+            push!(test_labels, 0);
+        else
+            push!(auxTrainImgs, image);
+            push!(train_labels, 0);
+        end
+        n += 1;
+    end
+
+    for file in readdir(dirPos)
+        image = load("$dirPos$file");
+        if (n % testRatio == 0) 
+            push!(auxTestImgs, image);
+            push!(test_labels, 1);
+        else
+            push!(auxTrainImgs, image);
+            push!(train_labels, 1);
+        end
+        n += 1;
+    end
+
+    return auxTrainImgs, auxTestImgs;
+    
+end
+
+train_imgs, test_imgs = loadImages(testRatio);
+
+
+
+
+
+function convertirArrayImagenesHWCN(imagenes)
+    numPatrones = length(imagenes);
+    nuevoArray = Array{Float32,4}(undef, 100, 75, 1, numPatrones); # Importante que sea un array de Float32
+    for i in 1:numPatrones
+        @assert (size(imagenes[i])==(100,75)) "Las imagenes no tienen tamaño 75*100";
+        nuevoArray[:,:,1,i] .= imagenes[i][:,:];
+    end;
+    return nuevoArray;
+end;
+train_imgs = convertirArrayImagenesHWCN(train_imgs);
+test_imgs = convertirArrayImagenesHWCN(test_imgs);
+
+println("Tamaño de la matriz de entrenamiento: ", size(train_imgs))
+println("Tamaño de la matriz de test:          ", size(test_imgs))
+
+
+# Cuidado: en esta base de datos las imagenes ya estan con valores entre 0 y 1
+# En otro caso, habria que normalizarlas
+println("Valores minimo y maximo de las entradas: (", minimum(train_imgs), ", ", maximum(train_imgs), ")");
+
+
+batch_size = 93;
+# Creamos los indices: partimos el vector 1:N en grupos de batch_size
+gruposIndicesBatch = Iterators.partition(1:size(train_imgs,4), batch_size);
+println("Se han creado ", length(gruposIndicesBatch), " grupos de indices para distribuir los patrones en batches");
+
+train_set = [ (train_imgs[:,:,:,indicesBatch], onehotbatch(train_labels[indicesBatch], labels)) for indicesBatch in gruposIndicesBatch];
+
+
+test_set = (test_imgs, onehotbatch(test_labels, labels));
+
+
+train_imgs = nothing;
+test_imgs = nothing;
+GC.gc();
+
+
+funcionTransferenciaCapasConvolucionales = relu;
+
+# Definimos la red con la funcion Chain, que concatena distintas capas
+ann = Chain(
+    Conv((1, 1), 1=>32, pad=(1,1), funcionTransferenciaCapasConvolucionales),
+    MaxPool((2,2)),
+    Conv((1, 1), 32=>32, pad=(1,1), funcionTransferenciaCapasConvolucionales),
+    MaxPool((2,2)),
+    Conv((1, 1), 32=>64, pad=(1,1), funcionTransferenciaCapasConvolucionales),
+    MaxPool((2,2)),
+    x -> reshape(x, :, size(x, 4)),
+    Dense(9856, 2),
+    softmax
+)
+
+
+numBatchCoger = 1; numImagenEnEseBatch = [12, 6];
+
+entradaCapa = train_set[numBatchCoger][1][:,:,:,numImagenEnEseBatch];
+numCapas = length(Flux.params(ann));
+println("La RNA tiene ", numCapas, " capas:");
+for numCapa in 1:numCapas
+    println("   Capa ", numCapa, ": ", ann[numCapa]);
+    # Le pasamos la entrada a esta capa
+    global entradaCapa # Esta linea es necesaria porque la variable entradaCapa es global y se modifica en este bucle
+    capa = ann[numCapa];
+    salidaCapa = capa(entradaCapa);
+    println("      La salida de esta capa tiene dimension ", size(salidaCapa));
+    entradaCapa = salidaCapa;
+end
+
+ann(train_set[numBatchCoger][1][:,:,:,numImagenEnEseBatch]);
+
+loss(ann, x, y) = (size(y,1) == 1) ? Losses.binarycrossentropy(ann(x),y) : Losses.crossentropy(ann(x),y);
+# Para calcular la precisión, hacemos un "one cold encoding" de las salidas del modelo y de las salidas deseadas, y comparamos ambos vectores
+accuracy(batch) = mean(onecold(ann(batch[1])) .== onecold(batch[2]));
+
+println("Ciclo 0: Precision en el conjunto de entrenamiento: ", 100*mean(accuracy.(train_set)), " %");
+
+# Optimizador que se usa: ADAM, con esta tasa de aprendizaje:
+opt_state = Flux.setup(Adam(0.001), ann);
+
+# Optimizador que se usa: ADAM, con esta tasa de aprendizaje:
+
+
+
+println("Comenzando entrenamiento...")
+mejorPrecision = -Inf;
+criterioFin = false;
+numCiclo = 0;
+numCicloUltimaMejora = 0;
+mejorModelo = nothing;
+
+while !criterioFin
+
+    # Hay que declarar las variables globales que van a ser modificadas en el interior del bucle
+    global numCicloUltimaMejora, numCiclo, mejorPrecision, mejorModelo, criterioFin;
+
+    # Se entrena un ciclo
+    Flux.train!(loss, ann, train_set, opt_state);
+
+    numCiclo += 1;
+
+    # Se calcula la precision en el conjunto de entrenamiento:
+    precisionEntrenamiento = mean(accuracy.(train_set));
+    println("Ciclo ", numCiclo, ": Precision en el conjunto de entrenamiento: ", 100*precisionEntrenamiento, " %");
+
+    # Si se mejora la precision en el conjunto de entrenamiento, se calcula la de test y se guarda el modelo
+    if (precisionEntrenamiento >= mejorPrecision)
+        mejorPrecision = precisionEntrenamiento;
+        precisionTest = accuracy(test_set);
+        println("   Mejora en el conjunto de entrenamiento -> Precision en el conjunto de test: ", 100*precisionTest, " %");
+        mejorModelo = deepcopy(ann);
+        numCicloUltimaMejora = numCiclo;
+    end
+
+    # Si no se ha mejorado en 5 ciclos, se baja la tasa de aprendizaje
+  #if (numCiclo - numCicloUltimaMejora >= 5) && (opt_state.eta > 1e-6)
+  #    opt_state.eta /= 10.0
+  #    println("   No se ha mejorado en 5 ciclos, se baja la tasa de aprendizaje ");
+  #    numCicloUltimaMejora = numCiclo;
+  #end
+
+    # Criterios de parada:
+
+    # Si la precision en entrenamiento es lo suficientemente buena, se para el entrenamiento
+    if (precisionEntrenamiento >= 0.999)
+        println("   Se para el entenamiento por haber llegado a una precision de 99.9%")
+        criterioFin = true;
+    end
+
+    # Si no se mejora la precision en el conjunto de entrenamiento durante 10 ciclos, se para el entrenamiento
+    if (numCiclo - numCicloUltimaMejora >= 10)
+        println("   Se para el entrenamiento por no haber mejorado la precision en el conjunto de entrenamiento durante 10 ciclos")
+        criterioFin = true;
+    end
+end
